247e51e61d2abacd74bbdf4be9e55afa7cecb5ff
Abastro <abab9579@gmail.com>
2018-12-11 23:36:09 +0900

    More progress, thinking on the interop

--- interpreter/Interpreter.hs
+++ interpreter/Interpreter.hs
@@ -17,213 +17,65 @@ instance Description Desc where
 
 data Void
 
---class Exchangeable e where
---    exchange :: Name -> e
+class Mapping m where
+    mapping :: m -> m -> Desc m
 
-class LambdaLike l where
-    evaluate :: l -> [l] -> Desc l
-    substitute :: Map.Map Name l -> l -> Desc l
-
-instance LambdaLike Void where
-    evaluate x y = x
-    substitute m p = p
-
-data Lambda value = Value value | Ref Name [Lambda value] | Lam Name (Lambda value)
-
-instance (LambdaLike expr) => LambdaLike (Lambda expr) where
-    evaluate (Lam pName content) vals = substitute (Map.singleton pName vals) content
-    evaluate (Ref pName list) vals = Ref pName (list vals)
-
-    substitute rules (Lam pName content) = if collision then describeError "Re-declared" else Lambda pName (expand rules content)
-    substitute rules (Ref pName list) = maybe (Ref pName list) (evaluate t list) ( pName rules)
+class (Mapping ev) => Evaluable ev where
+    replace :: Int -> ev -> ev -> Desc ev
 
 
-class FPLike fp where
-    pair :: fp -> fp -> Desc fp
-    extract :: fp -> Desc fp
-    apply :: fp -> Desc fp
+data Lambda expr = Val expr | Ref Int [Lambda expr] | Lam Int (Lambda expr)
 
-data FuncPair ext = FPExt ext | Func (LFP ext) | Pair (LFP ext) ext
-type LFP ext = Lambda (FuncPair ext)
+instance (Evaluable expr) => Mapping Lambda expr where
+    mapping ev dep = (evalLambda ev dep : [])
 
-instance (LambdaLike ext) => LambdaLike (FuncPair ext) where
-    evaluate _ _ = describeError ""
+instance (Evaluable expr) => Evaluable Lambda expr where
+    replace iref dep (Ref r pars) =
+        let postP = map (replace iref dep) list
+        in if iref == r then evalLambda dep postP else Ref r postP
+    replace iref dep (Lam r exp) =
+        if iref == r
+            then describeError "Declared Twice"
+            else Lam r (replace iref dep exp)
 
-    substitute rules (FPExt other) = FPExt (substitute rules other)
-    substitute rules (Func lambda) = Func (substitute rules lambda)
-    substitute rules (Pair lambda dep) = Pair (substitute rules lambda) (substitute rules dep)
-
-instance (FPLike ext) => FPLike LFP ext where
-    pair (Value value) _ = describeError ""
-    pair lam dep = Value (Pair lam dep)
-
-    extract (Value val) = extract val
-    extract _ = describeError ""
-
-    apply (Pair lambda dep) = evaluate lambda [dep]
-    apply _ = describeError ""
-
-classify :: Lambda Void -> LFP Void
-classify (Lam pName content) = Lam pName (classify content)
-classify (Ref pName list) = 
+evalLambda :: (Evaluable expr) => Lambda expr -> [Lambda expr] -> Desc (Lambda expr)
+evalLambda (Lam ref sub) (val : rest) = evalLambda (replace ref val sub)
+evalLambda (Ref r list) (v : vals) = Ref r (list (v : vals))
+evalLambda l [] = l
 
 
-class Typed tp where
-    typeOf :: tp -> tp
-
-data PSFlag = PiType | SigmaType
-data Types ext = TypeUni Int | PiSigma PSFlag (Types ext) (LT ext) | CustomType ext
-type LT ext = Lambda (Types ext)
-
-instance Typed (Typed ext) where
-    typeOf (TypeUni n) = TypeUni (n + 1)
-    typeOf _ = TypeUni 0
-
-data Clause expr = Type (Types expr) | TypedExpr (Clause expr) expr
-
-instance Typed Clause expr where
-    typeOf (Type t) = typeOf t
-    typeOf (TypedExpr t raw) = t
-
-instance (LambdaLike expr) => LambdaLike Clause expr where
-    evaluate _ _ = describeError ""
-    substitute rules (Type t) = Type (substitute rules t)
-    substitute rules (TypedExpr t raw) = TypedExpr (substitute rules t) (substitute rules raw)
-
-instance (FuncPair expr) => FuncPair Clause expr where
-    pair (TypedExpr cl ex) = 
+data FuncPair ext = External ext | FPL (Lambda (FuncPair ext)) | Pair (FuncPair ext) (FuncPair ext) | Extract (FuncPair ext) | Apply (FuncPair ext)
 
 
-data BlockN expr = Empty | BlockS (Block expr) (Name, expr)
-
-data ExPair expr = ExPair expr expr
-
-instance Pair (ExPair expr) expr where
-    extract (ExPair ev dep) = pure $ dep
-    funcIn (ExPair ev dep) = pure $ ev
-
-instance (Expandable expr) => Expandable (ExPair expr) expr where
-    expand rules (ExPair ev dep) = ExPair (expand rules ev) (expand rules dep)
+instance Mapping FuncPair ext where
+    mapping (FPL lambda) dep = ...
 
 
-data Operate expr = Apply expr | Extract expr
+pair :: (Mapping ext) => FuncPair ext -> FuncPair ext -> FuncPair ext
+pair fun dep = Pair fun dep
 
-instance (Function expr expr, Pair expr expr, Expandable expr) => Expandable (Operate expr) expr where
-    expand rules (Apply content) = let pair = expand rules content
-        in apply (funcIn pair) (extract pair)
-    expand rules (Extract content) = extract (expand rules content)
+extract :: FuncPair -> FuncPair
+extract (Pair fun dep) = dep
+extract pair = Extract pair
 
-
-data Ref = Ref name
-
-instance Expandable Ref expr where
-    expand rules (Ref name) = lookup rules name
-
-data Basis expr = FromLambda (Lambda expr) | FromExPair (ExPair expr) | FromOperation (Opearate expr) | FromRef Ref
-
-instance (Function expr expr, Pair expr expr, Expandable expr) => Expandable (Operate expr) expr where
-    expand rules (FromLambda l) = FromLambda (expand rules l)
-    expand rules (FromExPair p) = FromExPair (expand rules p)
-    expand rules (FromOperation opn) = FromOperation (expand rules opn)
-    expand rules (FromRef ref) = FromRef (expand rules ref)
-
--- Typed description
+apply :: FuncPair -> FuncPair
+apply (Pair (External ext) dep) = mapping ext dep
+apply (Pair (FPL lambda) dep) = mapping lambda dep
+apply pair = Apply pair
 
 
--- Evaluation process
-data Extern = ExtName Name
+data PSFlag = Pi | Sigma
+data DepType = TypeUni Int | Depend PSFlag Types (Lambda Types) | Extern Name
+
+data Clause ext = Typer DepType | Typed DepType (FuncPair ext)
 
 
-type ID = Name
-data EvalMapping = EL ID Evaluated | EB (Map.Map Name Evaluated) deriving Show
-data EvalConnect = EC Evaluated Evaluated deriving Show
-
-data EvalContext = DepRef ID | ExtEv Extern | EF EvalMapping | EP EvalConnect deriving Show
-
-data Evaluated = ExEval EvalContext | ImEval Evaluated Operation deriving Show
-type Intermediate = (Evaluated, Set.Set ID)
-
-evaluate :: Clause -> Maybe Intermediate
-evaluate clause = case clause of
-    Implicit param op -> do
-        (evSub, holes) <- evaluate param
-        ev <- case op of
-            Apply -> apply evSub
-            Extract -> extract evSub
-            _ -> return $ ImEval evSub op
-        return $ (ev, holes)
-    Explicit _ context -> do
-        (evContext, holes) <- case context of
-            IntRef name -> return (DepRef name, Set.singleton name)
-            ExtRef name -> return (ExtEv $ ExtName name, Set.empty) -- This is external, anyway
-            CFunc mapping -> case mapping of
-                Lambda name expr -> do
-                    (evSub, holeSub) <- evaluate expr
-                    return (EF $ EL name evSub, Set.delete name holeSub)
-                Block map -> do
-                    blockContent <- foldl comb (return Map.empty) map
-                    return (EF $ EB $ Map.map fst blockContent, foldl Set.union Set.empty $ Map.map snd blockContent)
-            CPair (Con fn dep) -> do
-                (evFn, fnh) <- evaluate fn
-                (evDep, deph) <- evaluate dep
-                return (EP $ EC evFn evDep, Set.union fnh deph)
-        return (ExEval evContext, holes)
-
-comb :: Maybe (Map.Map Name Intermediate) -> (Name, Clause) -> Maybe (Map.Map Name Intermediate)
-comb prev (name, expr) = do
-    stored <- prev
-    (evCur, ids) <- evaluate expr
-    evEnd <- let subs bid acc = maybe acc ((>>=) acc) $ fmap (substitute bid) (fmap fst $ Map.lookup bid stored)
-        in foldr subs (return evCur) ids
-    let idsAfter = foldl Set.union Set.empty $ Set.map (\someId -> maybe (Set.singleton someId) snd $ Map.lookup someId stored) ids
-        in let subs2 (evSub, holes) = fmap (,) (substitute name evEnd evSub) >>= pure . flip ($) (Set.union idsAfter $ Set.delete name holes)
-            in traverse subs2 (Map.insert name (evEnd, idsAfter) stored)
+impSimple :: Name -> a -> Clause a
+impSimple tName ext = Typed (Extern tName) (External ext)
 
 
-apply :: Evaluated -> Maybe Evaluated
-apply pair = do
-    EC ev dep <- disassemblePair pair
-    case ev of
-        ExEval context -> case context of
-            EF mapping -> case mapping of
-                EL did expr -> substitute did dep expr
-                EB map -> case dep of
-                    ExEval shouldBeName -> case shouldBeName of
-                        ExtEv maybeName -> case maybeName of
-                            ExtName name -> Map.lookup name map
-                            _ -> Nothing
-                        _ -> Nothing
-                    ImEval _ _ -> Nothing
-            DepRef _ -> return $ ImEval pair Apply
-            _ -> Nothing
-        ImEval intern op -> case op of
-            Apply -> return $ ImEval pair Apply
-            Extract -> return $ ImEval pair Apply
-            _ -> Nothing
+instance Functor Clause where
+    map conv cl = (conv p) -- (Here comes all the application, but Idk how)
 
-extract :: Evaluated -> Maybe Evaluated
-extract pair = fmap ext $ disassemblePair pair where
-    ext (EC fn dep) = dep
-
-disassemblePair :: Evaluated -> Maybe EvalConnect
-disassemblePair (ExEval context) = case context of
-    EP connect -> Just connect
-    _ -> Nothing
-disassemblePair _ = Nothing
-
--- Map based version?
-substitute :: ID -> Intermediate -> Evaluated -> Maybe Intermediate
-substitute did (dep, holeDep) expr = case expr of
-    ExEval context -> case context of
-        DepRef rid -> return $ if did == rid then (dep, holeDep) else (expr, Set.singleton rid)
-        ExtEv _ -> return expr
-        EF mapping -> fmap (ExEval . EF) $ case mapping of
-            EL otherId evaluated -> if otherId == did then Nothing else fmap (EL otherId) $ subsSame evaluated
-            EB map -> fmap EB $ traverse subsSame map
-        EP connect -> fmap (ExEval . EP) $ case connect of
-            EC func depen -> liftA2 EC (subsSame func) (subsSame depen)
-    ImEval inter op -> case op of
-        Apply -> subsSame inter >>= \app -> apply $ ImEval app op
-        Extract -> subsSame inter >>= \ext -> extract $ ImEval ext op
-        _ -> return expr
-    where subsSame = substitute did (dep, holeDep)
+instance Applicative Clause where
+    (<*>) clConv clDep = 
